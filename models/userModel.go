dda7994cee6c0245317076e1d9892116200f3615
afaris01 <sidukertas0@gmail.com>
2022-06-24 20:00:50 +0700

    update

--- models/userModel.go
+++ models/userModel.go
@@ -13,9 +13,9 @@ const (
 type User struct {
 	ID       int    `gorm:"primaryKey" json:"id"`
 	FullName string `gorm:"not null;uniqueIndex" json:"full_name" form:"full_name" valid:"required~Title is required"`
-	Email    string `gorm:"not null;uniqueIndex" json:"email" form:"email" valid:"reaquired~Email is reaquired,email~Email is invalid"`
+	Email    string `gorm:"not null;uniqueIndex" json:"email" form:"email" valid:"required~Email is required,email~Email is invalid"`
 	Password string `gorm:"not null" json:"password" form:"password" valid:"required~Password is required,minstringlength(6)~Password has to have minimum length 6 characters"`
-	Role     string `gorm:"not null;default:'member'" json:"role" form:"role" valid:"required~Role is requiered"`
+	Role     string `gorm:"not null;default:'member'" json:"role" form:"role" valid:"required~Role is required"`
 	TimeModel
 }
 
