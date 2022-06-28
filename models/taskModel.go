f8c2a2219e5eff5779db096768a61b3c108becd0
afaris01 <sidukertas0@gmail.com>
2022-06-28 21:39:11 +0700

    update

--- models/taskModel.go
+++ models/taskModel.go
@@ -9,7 +9,6 @@ type Task struct {
 	Title       string `gorm:"not null" json:"title" form:"title" valid:"required~Title is required"`
 	Description string `gorm:"not null" json:"description" form:"description" valid:"required~Description is required"`
 	Status      bool   `gorm:"not null" json:"status" form:"status" valid:"required~Status is required"`
-	Role        string `gorm:"not null" json:"role" form:"role" valid:"required~Role is requiered"`
 	UserID      int
 	User        *User
 	CategoryID  int
